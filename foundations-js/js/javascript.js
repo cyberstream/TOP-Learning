a7d4d39dbcdc57b181f126a52d4532f235f358ac
Eli Mitchell <985041+cyberstream@users.noreply.github.com>
2022-03-12 22:17:14 -0900

    Fix formatting issues and disable old func tests

--- foundations-js/js/javascript.js
+++ foundations-js/js/javascript.js
@@ -1,33 +1,39 @@
 // Define functions
 
 function add7(num) {
-    return num + 7;
+  return num + 7;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+  return num1 * num2;
 }
 
 function capitalize(text) {
-    stringLength = text.length;
+  const stringLength = text.length;
 
-    if(stringLength > 1) {
-        return text.substr(0,1).toUpperCase() + text.substr(1);
-    } else if (stringLength == 1) {
-        return text.toUpperCase();
-    } else return text;    
+  if (stringLength > 1) {
+    return text.substr(0,1).toUpperCase() + text.substr(1);
+  }
+
+  if (stringLength === 1) {
+    return text.toUpperCase();
+  }
+
+  return text;
 }
 
 function lastLetter(text) {
-    return text.substr(-1);
+  return text.substr(-1);
 }
 
 // Test functions
 
-console.log('Add 7 to 10: ' + add7(10));
-console.log('Multiply 3 * 6: ' + multiply(3, 6));
-console.log('Capitalize \"penguin\": ' + capitalize('penguin'));
-console.log('Capitalize \"pEnGuIn\": ' + capitalize('pEnGuIn'));
-console.log('Capitalize \"PENGUIN\": ' + capitalize('PENGUIN'));
-console.log('Capitalize \"p\": ' + capitalize('p'));
-console.log('Last letter of \"Old MacDonald\": ' + lastLetter('Old MacDonald'));
+/*
+console.log(`Add 7 to 10: ${add7(10)}`);
+console.log(`Multiply 3 * 6: ${multiply(3, 6)}`);
+console.log(`Capitalize "penguin": ${capitalize('penguin')}`);
+console.log(`Capitalize "pEnGuIn": ${capitalize('pEnGuIn')}`);
+console.log(`Capitalize "PENGUIN": ${capitalize('PENGUIN')}`);
+console.log(`Capitalize "p": ${capitalize('p')}`);
+console.log(`Last letter of "Old MacDonald": ${lastLetter('Old MacDonald')}`);
+*/
