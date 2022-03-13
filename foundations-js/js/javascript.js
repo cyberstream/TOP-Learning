a2097dee58075aead42308820405be07cf4baed4
Eli Mitchell <985041+cyberstream@users.noreply.github.com>
2022-03-12 22:30:00 -0900

    Add DOM manipulation practice

--- foundations-js/js/javascript.js
+++ foundations-js/js/javascript.js
@@ -37,3 +37,39 @@ console.log(`Capitalize "PENGUIN": ${capitalize('PENGUIN')}`);
 console.log(`Capitalize "p": ${capitalize('p')}`);
 console.log(`Last letter of "Old MacDonald": ${lastLetter('Old MacDonald')}`);
 */
+
+// DOM manipulation practice
+
+const p1 = document.createElement('div');
+
+// Add red paragraph
+const content = document.querySelector('div.content');
+
+p1.innerText = 'Hey I\'m red!';
+p1.style.color = 'red';
+
+content.appendChild(p1);
+
+// Add blue heading
+const h3 = document.createElement('h3');
+
+h3.innerText = 'I\'m a blue h3!';
+h3.style.color = 'blue';
+
+content.appendChild(h3);
+
+// Add div with child elements
+
+const div = document.createElement('div');
+const h1 = document.createElement('h1');
+const p2 = document.createElement('p');
+
+div.style.cssText = 'border: 1px solid black; background-color: pink';
+
+h1.innerHTML = 'I\'m in a div';
+p2.innerHTML = 'ME TOO!';
+
+div.appendChild(h1);
+div.appendChild(p2);
+
+content.appendChild(div);
